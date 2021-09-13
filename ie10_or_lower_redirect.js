c55b707f32b873718ee63780d8e5681adbe31169
Alex De Angelis <alex@activeimage.co.uk>
2021-09-13 17:11:39 +0100

    browser redirects update

--- ie10_or_lower_redirect.js
+++ ie10_or_lower_redirect.js
@@ -1,9 +1,23 @@
-(function($) {
+(function ($) {
+  if (navigator.userAgent.indexOf("MSIE") >= 0) {
+    //document.write("You are using an unsupported version of Internet Explorer. Please upgrade to Internet Explorer 11 or use a different web browser.");
+    //console.log("You are using an unsupported version of Internet Explorer. Please upgrade to Internet Explorer 11 or use a different web browser.");
+    window.location.href = "/_fallback/";
+  }
+})(jQuery);
 
-	if (navigator.userAgent.indexOf("MSIE") >= 0) {
-		//document.write("You are using an unsupported version of Internet Explorer. Please upgrade to Internet Explorer 11 or use a different web browser.");
-		//console.log("You are using an unsupported version of Internet Explorer. Please upgrade to Internet Explorer 11 or use a different web browser.");
-		window.location.href = "/_fallback/";
-	}
+/*Redirect if old IE browser (IE11 or older)*/
+var agentHeader = navigator.userAgent;
+var browserName = navigator.appName;
 
-})( jQuery );
+if (window.location.href !== "https://andersenfox.co.uk/upgrade-browser/") {
+  if (
+    browserName.indexOf("Microsoft Internet Explorer") !== -1 ||
+    agentHeader.indexOf("Trident/7.") !== -1
+  ) {
+    window.location.href = "https://andersenfox.co.uk/upgrade-browser/";
+  } else {
+    console.log("You are on a compatible browser");
+  }
+} else {
+}
